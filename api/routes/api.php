cde27103e3ca63d839a633ecc16f3e40ace89d15
nikola_valjarevic <nikolavaljarevic@gmail.com>
2024-08-18 15:37:35 +0200

    route

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\ProjectFilesController;
+use App\Http\Controllers\ProjectSharedController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +18,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', [LoginController::class, 'login']);
+Route::post('register', [LoginController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('projects', [ProjectController::class, 'store']);
+    Route::put('projects/{id}', [ProjectController::class, 'update']);
+    Route::delete('projects/{id}', [ProjectController::class, 'destroy']);
+    Route::get('projects/user/{userId}', [ProjectController::class, 'searchByUser']);
+    Route::get('projects/search', [ProjectController::class, 'searchByName']);
+    Route::post('logout', [LoginController::class, 'logout']);
+    Route::apiResource('project-files', ProjectFilesController::class)->only(['index', 'show', 'store', 'destroy']);
+    Route::get('project-files/project/{projectId}', [ProjectFilesController::class, 'findByProject']);
+    Route::get('paginate-files', [ProjectFilesController::class, 'paginateFiles']);
+    Route::get('shared-with-me/{userId}', [ProjectSharedController::class, 'sharedWithMe']);
+    Route::post('project-shared', [ProjectSharedController::class, 'store']);
+    Route::get('projects', [ProjectController::class, 'index']);
+    Route::get('projects/{id}', [ProjectController::class, 'show']);
 });
