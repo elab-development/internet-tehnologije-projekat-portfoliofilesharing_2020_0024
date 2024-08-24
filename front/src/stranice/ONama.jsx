ad6967455247dba3498550e44aeb76f5d810f3ed
nikola_valjarevic <nikolavaljarevic@gmail.com>
2024-08-24 17:50:37 +0200

    javni web servis

--- front/src/stranice/ONama.jsx
+++ front/src/stranice/ONama.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Naslov from "../komponente/Naslov";
 import {Col, Row} from "react-bootstrap";
 import nikola from "../slike/nikola.jpg";
 import marko from "../slike/marko.enc";
+import server from "../server";
 
 const ONama = () => {
 
@@ -20,6 +21,22 @@ const ONama = () => {
             tekst: "Marko je student cetvrte godine Fakulteta organizacionih nauka u Beogradu. Marko je zavrsio kurs za management i razvija svoje menadzment sposobnosti. Markov san je ovaj sajt postane go-to mesto za sve one koji zele da dele svoje projekte."
         }
     ];
+
+    const [prijateljSajta, setPrijateljSajta] = React.useState(null);
+
+    useEffect(() => {
+        server.get('https://randomuser.me/api/').then((response) => {
+            console.log(response);
+            let podaci = response.data.results[0];
+            setPrijateljSajta({
+                ime: podaci.name.first + ' ' + podaci.name.last,
+                slika: podaci.picture.large,
+                tekst: 'Ovaj sajt mi je dosta pomogao u razvoju mog projekta. Hvala! Oduvek ste mi prvi u srcu'
+            });
+        }).catch((error) => {
+            console.log(error);
+        });
+    }, []);
 
     return (
         <>
@@ -40,6 +57,18 @@ const ONama = () => {
                         );
                     })
                 }
+
+            {
+                prijateljSajta && (
+                    <Row>
+                        <Col>
+                            <h1>{prijateljSajta.ime}</h1>
+                            <img src={prijateljSajta.slika} alt={prijateljSajta.ime} className="img-thumbnail" />
+                            <p>{prijateljSajta.tekst}</p>
+                        </Col>
+                    </Row>
+                )
+            }
         </>
     );
 };
